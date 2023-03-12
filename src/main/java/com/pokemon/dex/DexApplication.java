65f16c2aa076994d3876daceb47d0a9df825a6a5
Bruno Pimentel <brunoapimentel@gmail.com>
2023-03-12 18:40:12 -0300

    Estrutura inicial da aplicação Spring

---
+++ src/main/java/com/pokemon/dex/DexApplication.java
@@ -0,0 +1,13 @@
+package com.pokemon.dex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DexApplication.class, args);
+	}
+
+}
